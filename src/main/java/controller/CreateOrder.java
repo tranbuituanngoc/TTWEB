6e887d91b477cd55f12cc86161ed766503a916d6
Trịnh Long Vũ <20130471@st.hcmuaf.edu.vn>
2023-05-17 11:10:54 +0700

    updade pom

--- src/main/java/controller/CreateOrder.java
+++ src/main/java/controller/CreateOrder.java
@@ -1,15 +1,12 @@
 package controller;
 
+import Util.Email;
 import model.CartUser;
 import model.Order;
 import model.ShippingAdress;
-import Util.Email;
-import model.Product;
-
 import model.User;
 import service.CartService;
 import service.OrderService;
-
 import service.ShippingAddressService;
 import service.VoucherService;
 
@@ -36,29 +33,29 @@ public class CreateOrder extends HttpServlet {
         String voucherCode = request.getParameter("voucher");
         CartUser cartUser = (CartUser) request.getSession().getAttribute("cartUser");
         User user = (User) request.getSession().getAttribute("user");
-        if (user == null ){
+        if (user == null) {
             request.getSession().setAttribute("userInvalid", true);
             response.sendRedirect("CheckOut");
         }
         String paymentMethod_raw = request.getParameter("payment-method");
         Timestamp leadTime = null;
         int fee = 0;
-        int discount =0;
+        int discount = 0;
         if (leadTime_raw != null) leadTime = new Timestamp(Long.parseLong(leadTime_raw) * 1000L);
         if (fee_raw != null) fee = Integer.parseInt(fee_raw);
         if (voucherCode != null) discount = VoucherService.getVoucher(voucherCode).getDiscount();
-        int paymentMethod =0;
+        int paymentMethod = 0;
         if (paymentMethod_raw != null) paymentMethod = Integer.parseInt(paymentMethod_raw);
         order.setShipping_cost(fee);
         order.setShipping_time(leadTime);
-        order.setTotalPrice(fee+cartUser.getTotalValue()-discount);
+        order.setTotalPrice(fee + cartUser.getTotalValue() - discount);
         if (user != null) order.setUserID(user.getId_User());
         order.setOrderID(newId);
         order.setPaymentMethodId(paymentMethod);
         order.setVoucher_code(voucherCode);
         ShippingAdress shippingAdress = (ShippingAdress) request.getSession().getAttribute("shippingAddress");
         System.out.println(shippingAdress);
-        if (shippingAdress == null ){
+        if (shippingAdress == null) {
             String fullname = request.getParameter("billing_address[full_name]");
             String address = request.getParameter("billing_address[address1]");
             String email = request.getParameter("checkout_user[email]");
@@ -66,9 +63,9 @@ public class CreateOrder extends HttpServlet {
             String provinceId_raw = request.getParameter("selected_customer_shipping_province");
             String districtId_raw = request.getParameter("selected_customer_shipping_district");
             String wardId_raw = request.getParameter("selected_customer_shipping_ward");
-            int ward_id =0;
+            int ward_id = 0;
             int province_id = 0;
-            int district_id  = 0;
+            int district_id = 0;
             if (wardId_raw != null) ward_id = Integer.parseInt(wardId_raw);
             if (provinceId_raw != null) province_id = Integer.parseInt(provinceId_raw);
             if (districtId_raw != null) district_id = Integer.parseInt(districtId_raw);
@@ -80,21 +77,21 @@ public class CreateOrder extends HttpServlet {
             shippingAdress.setFullName(fullname);
             shippingAdress.setProvinceId(province_id);
             shippingAdress.setWardId(ward_id);
-            if(user != null) shippingAdress.setUserId(user.getId_User());
+            if (user != null) shippingAdress.setUserId(user.getId_User());
             System.out.println(shippingAdress);
-            if(user != null) ShippingAddressService.addShippingAddress(shippingAdress);
+            if (user != null) ShippingAddressService.addShippingAddress(shippingAdress);
         }
-        if(user!= null){
+        if (user != null) {
             CartService.setCartOrder(user.getId_User(), order.getOrderID());
         }
 
 
-        if (OrderService.updateQuantity(order) == -1 ){
+        if (OrderService.updateQuantity(order) == -1) {
             request.getSession().setAttribute("errorQuantity", true);
             response.sendRedirect("CheckOut");
             CartService.removeCartOrder(user.getId_User());
         }
-        if (OrderService.updateQuantity(order) == 1 ) {
+        if (OrderService.updateQuantity(order) == 1) {
             String idTransport = new RegisterTransport().registerTransport(shippingAdress.getDistrictId(), shippingAdress.getWardId());
             System.out.println(idTransport);
             order.setIdTransport(idTransport);
@@ -103,17 +100,11 @@ public class CreateOrder extends HttpServlet {
             request.getSession().setAttribute("order", order);
             System.out.println("success");
             response.sendRedirect("SuccessOrder");
-
-//                 request.setAttribute("msg", "Bạn đã đặt hàng thành công");
-//                 response.sendRedirect("ProductLists");
-//                OrderDetailService.insertOrderDetail(pro);
-                Email mail = new Email();
-                Email.sendMail(email,"TrueMart-Order", "TrueMart gach men cao cấp đã nhận được đơn đặt hàng của bạn");
-                Cart newCart = new Cart();
-//                newCart.commit(session);
-            }
-
+            Email mail = new Email();
+            mail.sendMail(user.getEmail(), "TrueMart-Order", "TrueMart gach men cao cấp đã nhận được đơn đặt hàng của bạn");
         }
+
+
     }
 
     @Override

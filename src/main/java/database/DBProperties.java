78dd4a28d0aae8a25c40c69281d9503b5124e9c4
tranbuituanngoc <20130337@st.hcmuaf.edu.vn>
2023-06-14 22:35:00 +0700

    Revert "Merge branch 'deploy' into main"
    This reverts commit 5c0f2f0d198bb0744ef7b2d77cb0835e912dea38, reversing
    changes made to eec291d5908c2bf60358e489d94f7972871b5bcd.

--- src/main/java/database/DBProperties.java
+++ src/main/java/database/DBProperties.java
@@ -9,7 +9,6 @@ public class DBProperties {
     static {
         try {
             prop.load(DBProperties.class.getClassLoader().getResourceAsStream("db.properties"));
-            prop.setProperty("useSSL", "false");
         } catch (IOException ioException) {
             ioException.printStackTrace();
         }

88bd6e350f19dd6bb999a0182b6cf926d49c7505
NguyenXuanThanhTrung <20130452@st.hcmuaf.edu.vn>
2023-06-16 02:39:12 +0700

    ghi log người dùng thanh toán thành công

--- src/main/java/controller/SuccessOrder.java
+++ src/main/java/controller/SuccessOrder.java
@@ -1,7 +1,9 @@
 package controller;
 
+import bean.Log;
 import model.Order;
 import model.Voucher;
+import org.jdbi.v3.core.Jdbi;
 import service.VoucherService;
 
 import javax.servlet.ServletException;
@@ -13,11 +15,14 @@ import java.io.IOException;
 
 @WebServlet(name = "SuccessOrder", value = "/SuccessOrder")
 public class SuccessOrder extends HttpServlet {
+    Jdbi jdbi = Jdbi.create("jdbc:mysql://localhost:3306/gachmen_shop", "root", "");
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         Order order = (Order) request.getSession().getAttribute("order");
 //        request.setAttribute("order", order);
 //        int paymentMethodId = order.getPaymentMethodId();
+        Log log = new Log(Log.INFO, order.getUserID(), "SuccessOrder", "Đặt hàng thành công", "SUCCESS");
+        log.insert(jdbi);
         String voucher_code = order.getVoucher_code();
         Voucher voucher = VoucherService.getVoucher(voucher_code);
         request.setAttribute("voucher", voucher);

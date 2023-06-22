2975d09f3b34d6b71a50ab9c72e347eb0c4aea9b
Trịnh Long Vũ <81180330+trinhvu1711@users.noreply.github.com>
2023-06-22 10:10:57 +0700

    fix add cart
    sửa thêm nhiều sp trong detail page

--- src/main/java/controller/AddCart.java
+++ src/main/java/controller/AddCart.java
@@ -28,7 +28,7 @@ public class AddCart extends HttpServlet {
         User user = (User) request.getSession().getAttribute("user");
 
         String quantity_raw = request.getParameter("quantity_value");
-
+        System.out.println(quantity_raw);
         int quantity  =1;
         if (quantity_raw != null){
             quantity = Integer.parseInt(quantity_raw);
@@ -74,6 +74,55 @@ public class AddCart extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String id = request.getParameter("product_id");
+        if (id == null){
+            id = request.getParameter("productID");
+        }
+        Product p = ProductService.getById(id);
+        String colorId_raw = request.getParameter("color_id");
+        User user = (User) request.getSession().getAttribute("user");
 
+        String quantity_raw = request.getParameter("quantity_value");
+        System.out.println(quantity_raw);
+        int quantity  =1;
+        if (quantity_raw != null){
+            quantity = Integer.parseInt(quantity_raw);
+        }
+        int colorId = p.getColor().get(0).getId_color();
+        if (colorId_raw != null) {
+            colorId = Integer.parseInt(colorId_raw);
+        }
+        String sizeId_raw = request.getParameter("size");
+        int sizeId = p.getSize().get(0).getIdSize();
+        if (sizeId_raw != null) {
+            sizeId = Integer.parseInt(sizeId_raw);
+        }
+
+        int price = ProductImportedService.getPrice(id, sizeId, colorId);
+        Cart cart = new Cart();
+        Color color = ProductColorService.getColorById(colorId);
+        Size size = ProductSizeService.getSizeById(sizeId);
+        p.setPrice(price);
+        cart.setColor(color);
+        cart.setSize(size);
+        cart.setProduct(p);
+        HttpSession session = request.getSession();
+        CartUser cartUser = (CartUser) session.getAttribute( "cartUser");
+        if (cartUser == null) {
+            cartUser = new CartUser();
+            if (user != null) cartUser.setIdUser(user.getId_User());
+        }
+        if (user != null) cartUser.setIdUser(user.getId_User());
+
+        cartUser.addCart(cart, quantity);
+        if (user != null) CartService.addCart(cartUser, cart, quantity);
+        session.setAttribute("cartUser", cartUser);
+        String url = request.getHeader("referer");
+
+        // Ghi log
+        Log log = new Log(Log.INFO, user != null ? user.getId_User() : "-1", "AddCart", "Thêm sản phẩm vào giỏ thành công", "success");
+        log.insert(jdbi);
+
+        response.sendRedirect(url);
     }
 }
